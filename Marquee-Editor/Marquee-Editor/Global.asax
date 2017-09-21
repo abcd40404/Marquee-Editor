76f0f9c54ff9404c2279954e26b1cf2279a7a711
秉辭 林 <abcd40404@outlook.com>
2017-09-21 23:59:04 +0800

    create project

---
+++ Marquee-Editor/Marquee-Editor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Marquee_Editor.MvcApplication" Language="C#" %>
---
+++ Marquee-Editor/Marquee-Editor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Marquee_Editor.Startup))]
+namespace Marquee_Editor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

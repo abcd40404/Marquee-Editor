fcda8ec25c5caede3d03a5bf3542b8f75393581f
秉辭 林 <abcd40404@outlook.com>
2017-10-06 17:57:02 +0800

    add model

---
+++ Marquee-Editor/Marquee-Editor/APIController/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Marquee_Editor.Models;
+
+
+namespace Marquee_Editor.APIController
+{
+    public class LoginController : ApiController
+    {
+
+    }
+}
---
+++ Marquee-Editor/Marquee-Editor/Models/MarqueeModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Marquee_Editor.Models
+{
+    public class MarqueeModels
+    {
+        public string Location { get; set; }
+
+        public string Station { get; set; }
+    }
+}

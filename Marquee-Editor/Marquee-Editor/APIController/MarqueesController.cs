d4cc300dea5fac21c1d4d891bf56ba74992d40d1
Lin Ping Tzu <abcd40404@yahoo.com.tw>
2017-10-07 17:38:39 +0800

    Add Controllers

---
+++ Marquee-Editor/Marquee-Editor/APIController/MarqueesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Marquee_Editor.Models;
+
+namespace Marquee_Editor.APIController
+{
+    public class MarqueesController : ApiController
+    {
+        private MyDB db = new MyDB();
+
+        // GET: api/Marquees
+        public IQueryable<Marquee> GetMarquees()
+        {
+            return db.Marquees;
+        }
+
+        // GET: api/Marquees/5
+        [ResponseType(typeof(Marquee))]
+        public IHttpActionResult GetMarquee(int id)
+        {
+            Marquee marquee = db.Marquees.Find(id);
+            if (marquee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(marquee);
+        }
+
+        // PUT: api/Marquees/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMarquee(int id, Marquee marquee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != marquee.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(marquee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MarqueeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Marquees
+        [ResponseType(typeof(Marquee))]
+        public IHttpActionResult PostMarquee(Marquee marquee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Marquees.Add(marquee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = marquee.Id }, marquee);
+        }
+
+        // DELETE: api/Marquees/5
+        [ResponseType(typeof(Marquee))]
+        public IHttpActionResult DeleteMarquee(int id)
+        {
+            Marquee marquee = db.Marquees.Find(id);
+            if (marquee == null)
+            {
+                return NotFound();
+            }
+
+            db.Marquees.Remove(marquee);
+            db.SaveChanges();
+
+            return Ok(marquee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MarqueeExists(int id)
+        {
+            return db.Marquees.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Marquee-Editor/Marquee-Editor/Controllers/MarqueesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Marquee_Editor.Models;
+
+namespace Marquee_Editor.Controllers
+{
+    public class MarqueesController : Controller
+    {
+        private MyDB db = new MyDB();
+
+        // GET: Marquees
+        public ActionResult Index()
+        {
+            return View(db.Marquees.ToList());
+        }
+
+        // GET: Marquees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marquee marquee = db.Marquees.Find(id);
+            if (marquee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marquee);
+        }
+
+        // GET: Marquees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marquees/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Location,Station")] Marquee marquee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Marquees.Add(marquee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(marquee);
+        }
+
+        // GET: Marquees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marquee marquee = db.Marquees.Find(id);
+            if (marquee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marquee);
+        }
+
+        // POST: Marquees/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Location,Station")] Marquee marquee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(marquee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(marquee);
+        }
+
+        // GET: Marquees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Marquee marquee = db.Marquees.Find(id);
+            if (marquee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marquee);
+        }
+
+        // POST: Marquees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Marquee marquee = db.Marquees.Find(id);
+            db.Marquees.Remove(marquee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
